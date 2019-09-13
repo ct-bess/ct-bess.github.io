bf8423ae9b63080f685caeb3d0c2871dedbc5143
ct-bess <ct_bess@gmail.com>
2019-09-12 20:06:37 -0500

    Uhh react?

---
+++ precache-manifest.18b54760042ae894e9b3dc16f967742d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "312b5aa72be159a94ef0be8368fcd599",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b922f5f08626814c3d03",
+    "url": "/static/css/main.a4e4d2bd.chunk.css"
+  },
+  {
+    "revision": "dabfe2912ae977c52652",
+    "url": "/static/js/2.6679d75c.chunk.js"
+  },
+  {
+    "revision": "b922f5f08626814c3d03",
+    "url": "/static/js/main.c18222ee.chunk.js"
+  },
+  {
+    "revision": "56cdfba2accd8594a5b1",
+    "url": "/static/js/runtime~main.a6aef668.js"
+  },
+  {
+    "revision": "d53e473cb560a53713d7ca702e3018b6",
+    "url": "/static/media/logo.d53e473c.jpeg"
+  },
+  {
+    "revision": "67f34ec83ba1c4350fff55d0792f5092",
+    "url": "/static/media/those-cards.67f34ec8.png"
+  }
+]);
